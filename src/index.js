acd5065a4b518260d20fe79a8787d146c49a221e
simonguo <simon.guo@hypers.com>
2018-08-31 13:47:01 +0800

    bump 0.1.1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-export flaser from './flaser';
-export parse from './parse';
+import _flaser from './flaser';
+import _parse from './parse';
+
+export const flaser = _flaser;
+export const parse = _parse;
+
+export default {
+  flaser,
+  parse
+};
